cf6646f322c1de9681020f0644066904ff91ce65
Nikorux <nicole.0126@hotmail.es>
2020-10-12 19:10:55 -0400

    Initial commit

---
+++ app/src/main/java/com/pucmm/tarea2_act_inten_permissions/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pucmm.tarea2_act_inten_permissions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/pucmm/tarea2_act_inten_permissions/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.pucmm.tarea2_act_inten_permissions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
